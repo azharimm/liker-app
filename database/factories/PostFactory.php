1c1d717b6a31e60373ca52e5fe2ff9254c42df1c
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-08-08 19:09:58 +0700

    + post model and migration

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'body' => $faker->sentence(10),
+    ];
+});
